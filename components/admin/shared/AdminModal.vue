6af3591494991ba2a37752fcdd17502b0e0bddb1
refaat1297 <a.refaat1297@gmail.com>
2020-11-01 19:30:32 +0200

    add admin modal

---
+++ components/admin/shared/AdminModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mt-8 mb-8">
+        <button
+            class="transition-all duration-300 ease-in-out text-white rounded bg-pink-500 px-8 py-3 font-sans font-semibold shadow-lg outline-none focus:outline-none hover:bg-pink-800"
+            @click="toggleModal = !toggleModal"
+        >
+            Open Modal
+        </button>
+
+        <div
+            v-if="toggleModal"
+            class="z-50 fixed flex justify-center items-center overflow-x-hidden overflow-y-auto inset-0"
+        >
+            <div class="relative mx-auto w-auto max-w-2xl">
+                <div class="bg-white w-full rounded p-5 shadow-2xl flex flex-col">
+                    <div class="text-2xl font-bold">Hello from modal</div>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates atque dolores error quo? Ab tempore omnis, rem a incidunt recusandae magni, pariatur harum expedita, officia amet quisquam facere asperiores autem!</span>
+                    <button class="rounded bg-green-500 text-white px-6 mt-1 py-2 w-3/12 m-auto" @click="toggleModal = false">Close</button>
+                </div>
+            </div>
+        </div>
+
+        <div
+            v-if="toggleModal"
+            class="transition-all duration-1000 ease-in-out absolute z-40 inset-0 opacity-25 bg-black"
+        ></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AdminModal",
+    data() {
+        return {
+            toggleModal: false
+        };
+    }
+};
+</script>
--- pages/admin/teachers/index.vue
+++ pages/admin/teachers/index.vue
@@ -2,8 +2,10 @@
     <div class="lg:container mx-auto px-4 mt-8">
         <h2 class="mb-5 flex items-center justify-between">
             <span class="text-3xl font-extrabold italic">All Teachers</span>
-            <button @click="getTeachers()">Add Teacher</button>
+            <button @click="open()">Add Teacher</button>
         </h2>
+        
+        <AdminModal />
 
         <div
             class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5"
@@ -18,27 +20,50 @@
 </template>
 
 <script>
-import {db} from '@/firebase'
+import { db } from "@/firebase";
 import TeacherCard from "@/components/admin/teachers/TeacherCard";
+
+import AdminModal from "@/components/admin/shared/AdminModal";
+
 
 export default {
     components: {
-        TeacherCard
+        TeacherCard,
+        AdminModal
     },
-    async asyncData () {
-        return db.collection('teachers').get().then(res => {
-            let teachers = res.docs.map(doc => {
-                return Object.assign({}, doc.data(), {
-                    id: doc.id
-                })
-            })
+    methods: {
+        open() {
+            this.$refs.modal.open();
+        }
+    },
+    async asyncData() {
+        return db
+            .collection("teachers")
+            .get()
+            .then(res => {
+                let teachers = res.docs.map(doc => {
+                    return Object.assign({}, doc.data(), {
+                        id: doc.id
+                    });
+                });
 
-            return {
-                teachers
-            }
-        })
+                return {
+                    teachers
+                };
+            });
     }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sweet-modal {
+    .sweet-box-actions {
+        .sweet-action-close {
+            display: flex !important;
+            justify-content: center !important;
+            align-items: center !important;
+            margin: auto !important;
+        }
+    }
+}
+</style>
