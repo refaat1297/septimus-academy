9850a7dc2daaf851f84d6fd5799fdb88f095df4b
refaat1297 <a.refaat1297@gmail.com>
2020-11-01 12:52:47 +0200

    add teachers page on admin layout

---
+++ components/admin/teachers/TeacherCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="md:flex bg-white rounded-lg p-6 bg-gray-300">
+        <img
+            class="h-16 w-16 md:h-24 md:w-24 rounded-full object-cover mx-auto md:mx-0 md:mr-6"
+            src="~/assets/img/teachers/teacher-1.jpg"
+        />
+        <div
+            class="text-center md:text-left flex flex-col sm:items-center md:items-start sm:justify-center"
+        >
+            <h2 class="text-lg font-bold italic text-xl">{{ teacher.name }}</h2>
+            <div class="text-purple-500 font-semibold">Product Engineer</div>
+            <div class="text-gray-800">{{ teacher.email }}</div>
+            <div class="text-gray-800">{{ teacher.phone }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TeacherCard",
+    props: {
+        teacher: {
+            type: Boolean,
+            required: true
+        }
+    }
+};
+</script>
--- pages/admin/teachers/index.vue
+++ pages/admin/teachers/index.vue
@@ -2,26 +2,59 @@
     <div class="lg:container mx-auto px-4 mt-8">
         <h2 class="mb-5 flex items-center justify-between">
             <span class="text-3xl font-extrabold italic">All Teachers</span>
-            <button>Add Teacher</button>
+            <button @click="getTeachers()">Add Teacher</button>
         </h2>
+
         <div
             class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5"
         >
-            <nuxt-link v-for="(num, i) in [1, 2, 3, 4, 5, 6, 7]" :key="i" to="/teacher" class="md:flex bg-white rounded-lg p-6 bg-gray-300">
-                <img
-                    class="h-16 w-16 md:h-24 md:w-24 rounded-full object-cover mx-auto md:mx-0 md:mr-6"
-                    src="~/assets/img/teachers/teacher-1.jpg"
-                />
-                <div class="text-center md:text-left flex flex-col sm:items-center md:items-start sm:justify-center">
-                    <h2 class="text-lg font-bold italic text-xl">Erin Lindford</h2>
-                    <div class="text-purple-500 font-semibold">Product Engineer</div>
-                    <div class="text-gray-800">erinlindford@example.com</div>
-                </div>
-            </nuxt-link>
-            
-            
+            <p v-if="$fetchState.pending">Fetching mountains...</p>
+            <p v-else-if="$fetchState.error">An error occurred :(</p>
+            <TeacherCard
+                v-else
+                v-for="teacher in teachers"
+                :key="teacher.email"
+                :teacher="teacher"
+            />
         </div>
     </div>
 </template>
+
+<script>
+import { db } from "@/firebase";
+
+import TeacherCard from "@/components/admin/teachers/TeacherCard";
+
+export default {
+    components: {
+        TeacherCard
+    },
+    data() {
+        return {
+            teachers: null
+        };
+    },
+    fetch() {
+        return db
+            .ref("teachers")
+            .once("value")
+            .then(data => {
+                console.log(data);
+                this.teachers = data.val();
+            });
+    },
+    methods: {
+        getTeachers() {
+            return db
+                .ref("teachers")
+                .once("value")
+                .then(data => {
+                    console.log(data);
+                    this.teacherss = data.val();
+                });
+        }
+    }
+};
+</script>
 
 <style lang="scss" scoped></style>
