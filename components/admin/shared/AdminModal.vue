a79dd12f362de37066ba0d6d10fe078e097ec713
refaat1297 <a.refaat1297@gmail.com>
2020-11-01 19:48:39 +0200

    edit admin modal component

--- components/admin/shared/AdminModal.vue
+++ components/admin/shared/AdminModal.vue
@@ -9,20 +9,45 @@
 
         <div
             v-if="toggleModal"
-            class="z-50 fixed flex justify-center items-center overflow-x-hidden overflow-y-auto inset-0"
+            class="z-50 transition-all duration-1000 ease-in-out fixed flex justify-center items-center overflow-x-hidden overflow-y-auto inset-0"
         >
-            <div class="relative mx-auto w-auto max-w-2xl">
-                <div class="bg-white w-full rounded p-5 shadow-2xl flex flex-col">
-                    <div class="text-2xl font-bold">Hello from modal</div>
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates atque dolores error quo? Ab tempore omnis, rem a incidunt recusandae magni, pariatur harum expedita, officia amet quisquam facere asperiores autem!</span>
-                    <button class="rounded bg-green-500 text-white px-6 mt-1 py-2 w-3/12 m-auto" @click="toggleModal = false">Close</button>
+            <div class="relative mx-auto w-auto max-w-2xl ml-3 mr-3">
+                <div
+                    class="bg-white w-full rounded p-5 shadow-2xl flex flex-col"
+                >
+                    <div class="modal-header">
+                        <h3 class="text-2xl font-bold">Hello from modal</h3>
+                    </div>
+                    <div class="modal-body">
+                        <span
+                            >Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Voluptates atque dolores error quo? Ab tempore
+                            omnis, rem a incidunt recusandae magni, pariatur
+                            harum expedita, officia amet quisquam facere
+                            asperiores autem!</span
+                        >
+                    </div>
+                    <div class="modal-footer flex justify-end items-center">
+                        <button
+                            class="rounded bg-red-500 text-white px-6 mt-1 py-2 mr-3"
+                            @click="toggleModal = false"
+                        >
+                            Close
+                        </button>
+                        <button
+                            class="rounded bg-green-500 text-white px-6 mt-1 py-2 "
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
 
         <div
             v-if="toggleModal"
-            class="transition-all duration-1000 ease-in-out absolute z-40 inset-0 opacity-25 bg-black"
+            @click="toggleModal = false"
+            class="transition-all duration-1000 ease-in-out absolute z-40 inset-0 opacity-50 bg-black"
         ></div>
     </div>
 </template>
