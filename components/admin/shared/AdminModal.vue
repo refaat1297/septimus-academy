b093a87fdd32bd01b107f69957663dc02342f995
refaat1297 <a.refaat1297@gmail.com>
2020-11-01 22:29:34 +0200

    added button to add new teacher

--- components/admin/shared/AdminModal.vue
+++ components/admin/shared/AdminModal.vue
@@ -1,12 +1,6 @@
 <template>
     <div class="mt-8 mb-8">
-        <button
-            class="transition-all duration-300 ease-in-out text-white rounded bg-pink-500 px-8 py-3 font-sans font-semibold shadow-lg outline-none focus:outline-none hover:bg-pink-800"
-            @click="toggleModal = !toggleModal"
-        >
-            Open Modal
-        </button>
-
+        
         <div
             v-if="toggleModal"
             class="z-50 transition-all duration-1000 ease-in-out fixed flex justify-center items-center overflow-x-hidden overflow-y-auto inset-0"
@@ -19,26 +13,7 @@
                         <h3 class="text-2xl font-bold">Hello from modal</h3>
                     </div>
                     <div class="modal-body">
-                        <span
-                            >Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Voluptates atque dolores error quo? Ab tempore
-                            omnis, rem a incidunt recusandae magni, pariatur
-                            harum expedita, officia amet quisquam facere
-                            asperiores autem!</span
-                        >
-                    </div>
-                    <div class="modal-footer flex justify-end items-center">
-                        <button
-                            class="rounded bg-red-500 text-white px-6 mt-1 py-2 mr-3"
-                            @click="toggleModal = false"
-                        >
-                            Close
-                        </button>
-                        <button
-                            class="rounded bg-green-500 text-white px-6 mt-1 py-2 "
-                        >
-                            Submit
-                        </button>
+                        <slot></slot>
                     </div>
                 </div>
             </div>
@@ -46,7 +21,6 @@
 
         <div
             v-if="toggleModal"
-            @click="toggleModal = false"
             class="transition-all duration-1000 ease-in-out fixed h-screen z-40 inset-0 opacity-50 bg-black"
         ></div>
     </div>
@@ -55,10 +29,11 @@
 <script>
 export default {
     name: "AdminModal",
-    data() {
-        return {
-            toggleModal: false
-        };
+    props: {
+        toggleModal: {
+            type: Boolean,
+            required: true
+        }
     }
 };
 </script>
--- pages/admin/teachers/index.vue
+++ pages/admin/teachers/index.vue
@@ -2,10 +2,69 @@
     <div class="lg:container mx-auto px-4 mt-8">
         <h2 class="mb-5 flex items-center justify-between">
             <span class="text-3xl font-extrabold italic">All Teachers</span>
-            <button @click="open()">Add Teacher</button>
+            <button
+                class="transition-all duration-300 ease-in-out text-white rounded bg-pink-500 px-8 py-3 font-sans font-semibold shadow-lg outline-none focus:outline-none hover:bg-pink-800"
+                @click="toggleModal = true"
+            >
+                Add button
+            </button>
         </h2>
-        
-        <AdminModal />
+
+        <AdminModal
+            :toggleModal="toggleModal"
+        >
+            <template v-slot>
+                <form @submit.prevent="submit()">
+                    <div>
+                        <p>name</p>
+                        <input
+                            type="text"
+                            placeholder="name"
+                            v-model="teacher.name"
+                        />
+                    </div>
+                    <div>
+                        <p>email</p>
+                        <input
+                            type="text"
+                            placeholder="email"
+                            v-model="teacher.email"
+                        />
+                    </div>
+                    <div>
+                        <p>phone</p>
+                        <input
+                            type="text"
+                            placeholder="phone"
+                            v-model="teacher.phone"
+                        />
+                    </div>
+                    <div>
+                        <p>specialty</p>
+                        <input
+                            type="text"
+                            placeholder="specialty"
+                            v-model="teacher.specialty"
+                        />
+                    </div>
+
+                    <div class="modal-footer flex justify-end items-center">
+                        <button
+                            class="rounded bg-red-500 text-white px-6 mt-1 py-2 mr-3"
+                            @click="close()"
+                        >
+                            Close
+                        </button>
+                        <button
+                            class="rounded bg-green-500 text-white px-6 mt-1 py-2 "
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </template>
+            <!-- <template v-slot:submit-text>Add </template> -->
+        </AdminModal>
 
         <div
             class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5"
@@ -25,15 +84,47 @@ import TeacherCard from "@/components/admin/teachers/TeacherCard";
 
 import AdminModal from "@/components/admin/shared/AdminModal";
 
-
 export default {
     components: {
         TeacherCard,
         AdminModal
     },
+    data() {
+        return {
+            toggleModal: false,
+            teacher: {
+                name: null,
+                email: null,
+                phone: null,
+                specialty: null
+            }
+        };
+    },
     methods: {
-        open() {
-            this.$refs.modal.open();
+        close() {
+            this.toggleModal = false;
+        },
+        submit() {
+            const form = {
+                ...this.teacher,
+                image: 'teacher-1.jpg'
+            }
+
+            console.log(form)
+
+            return db
+                .collection("teachers")
+                .add(form)
+                .then(res => {
+                    const newTeacher = {
+                        ...form,
+                        id: res.id
+                    }
+
+                    this.teachers.unshift(newTeacher)
+
+                    this.close()                    
+                });
         }
     },
     async asyncData() {
